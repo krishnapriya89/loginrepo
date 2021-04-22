350adbb33c034c2e631fcb5291eba4b1afe2d435
Your Name <you@example.com>
2021-04-22 14:11:14 +0530

    fifth

---
+++ resources/views/login.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{url('login.css')}}">
+
+</head>
+<body background="images/logbackim.jpg">
+    <h1 class="head1">Student Login Details</h1>
+    
+    
+    <!-- <img src="images/Pure-CSS3-Login-Form.jpg"   alt=""> -->
+    <!-- <hr width="50%" color="black">  -->
+    <form action="" method="GET"  >
+        <table  align="center"  cellspacing="10" cellpadding="10" >
+            <tr><td >LOGIN HERE</td></tr>
+            <br>
+<img src="{{url('images/logimlogo1.png')}}" class="img1" alt="">
+            <!-- <tr><td > username</td></tr> -->
+            <tr><td ><input type="text" placeholder="username" ></td></tr>
+            
+            <tr><td><input type="password" placeholder="password"> </td></tr>
+            
+            
+            <tr><td> <button >sign in</button><br><br><label for="" class="lb">forgot password?</label></td></tr>
+           <tr><td><button>reset password</button></td></tr>
+          
+        </table>
+    </form>
+</body>
+</html>
